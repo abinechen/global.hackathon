d3b5267a88faf8a64b22a65eb88dc881ca306934
Nickel Cheng <nickel.y.j.cheng@gmail.com>
2014-12-07 01:43:39 -0800

    list existing articles

--- Teams/Pusheen/pusheen/documents/views.py
+++ Teams/Pusheen/pusheen/documents/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from documents.models import Article
+
 # Create your views here.
+def listArticle(request):
+    articleList = Article.objects.all()
+    content = {'articleList': articleList}
+    return render(request, 'documents/listArticle.html', content)
+
