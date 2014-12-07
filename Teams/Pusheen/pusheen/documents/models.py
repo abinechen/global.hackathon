88d9b84ccbca68463670d66283120a088b6ff233
Nickel Cheng <nickel.y.j.cheng@gmail.com>
2014-12-07 01:47:31 -0800

    use char field store file path

--- Teams/Pusheen/pusheen/documents/models.py
+++ Teams/Pusheen/pusheen/documents/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     name = models.CharField(max_length=100)
-    path = models.FilePathField(blank=True)
+    path = models.CharField(max_length=200)
     def __unicode__(self):
         return self.name
 
