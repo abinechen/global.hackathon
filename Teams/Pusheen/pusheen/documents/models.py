332d0328a32ea8ae8be2b60835ecd438bdf27dd7
Nickel Cheng <nickel.y.j.cheng@gmail.com>
2014-12-06 23:53:41 -0800

    add representation for title and subtitle

--- Teams/Pusheen/pusheen/documents/models.py
+++ Teams/Pusheen/pusheen/documents/models.py
@@ -3,16 +3,23 @@ from django.db import models
 # Create your models here.
 class Title(models.Model):
     titleText = models.TextField()
+    articleID = models.ForeignKey(Article)
+    def __unicode__(self):
+        return self.titleText
 
 class SubTitle(models.Model):
     subTitleText = models.TextField()
     titleID = models.ForeignKey(Title)
     isImportant = models.BooleanField(default=False)
+    def __unicode__(self):
+        return self.subTitleText
 
 class Content(models.Model):
     content = models.TextField()
     subTitleID = models.ForeignKey(SubTitle)
     isImportant = models.BooleanField(default=False)
+    def __unicode__(self):
+        return self.contentText
 
 class Keyword(models.Model):
     startPos = models.IntegerField()
